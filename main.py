71311f0fa1aeb7f129a038f6163ac227f5f33100
Ulandt <ulanbek.dt94@gmail.com>
2020-10-09 22:40:37 +0600

    mysql changes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from bs4 import BeautifulSoup
 import re
-
-from django.contrib.sites import requests
+import mysql.connector
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from time import sleep
 
@@ -29,7 +28,10 @@ def get_all_categories(html):
 
 def get_all_pages(url_list):
     all_pages = []
-    for url in url_list:
+    for url in url_list[:2]:
+        '''
+        Необходимо убрать слайс для парсинга всего сайте
+        '''
         html = driver.page_source
         driver.get(url)
         soup = BeautifulSoup(html, 'lxml')
@@ -46,12 +48,39 @@ def get_all_pages(url_list):
 
 
 def get_data(url_list):
-    for url in url_list:
-        link = requests.get(url)
-        soup = BeautifulSoup(link, 'lxml')
-        items = soup.findAll('p').text
-        print(items)
+    data = {}
+    for url in url_list[:2]:
+        html = driver.page_source
+        driver.get(url)
+        soup = BeautifulSoup(html, 'lxml')
+        items = soup.findAll('p')
+        for item in items:
+            imya = item.find('b')
+            text = item.text
+            pattern = re.compile("\Цена: \d+")
+            cena = pattern.findall(text)
+            if len(cena) != 0 and imya != None:
+                title = str(imya)[3:-4]
+                price = cena[0]
+                data.update({title:price})
+        sleep(5)
+    return(data)
 
 
-get_data(get_all_pages(get_all_categories(html)))
+def save_data(json):
+    for i, j in zip(json.keys(), json.values()):
+        connection = mysql.connector.connect(host='localhost',
+                                             user='ulan',
+                                             password='password',
+                                             database='books')
+        cursor = connection.cursor()
+        insert_query = '''INSERT INTO books(title, price) VALUES(%s, %s)'''
+        record = (i, j)
+        cursor.execute(insert_query, record)
+        connection.commit()
+        print('Success')
+
+    cursor.close()
+
+save_data(get_data(get_all_pages(get_all_categories(html))))
 
